088b025d4c1874bc72edded999a7d62bf889f1fa
Aditya Sharma <saditya4063@gmail.com>
2026-08-21 13:49:02 +0530

    Update API base URL to production endpoint

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const api = axios.create({ baseURL: "http://localhost:5000/api" });
-// https://infracare.onrender.com/api
+const api = axios.create({ baseURL: "https://infracare.onrender.com/api" });
+// http://localhost:5000/api
 
 
 api.interceptors.request.use((config) => {
